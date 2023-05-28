1ab90a0c2c107970ccca303eb9b3e611dba913bd
Denny <dtran415@gmail.com>
2023-05-28 14:33:42 -0700

    Fixing crash in jobs when not logged in

--- frontend/src/job/Job.js
+++ frontend/src/job/Job.js
@@ -1,23 +1,23 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { Button, Card, CardBody } from "reactstrap";
 import UserContext from "../user/UserContext";
 import JoblyApi from "../api";
 
 function Job({ job }) {
     const { id, title, salary, equity, companyName } = job;
-    const {user, setUser} = useContext(UserContext);
-    const [isAppliedTo, setAppliedTo] = useState(user.applications.includes(id));
+    const { user, setUser } = useContext(UserContext);
+    const [isAppliedTo, setAppliedTo] = useState(user ? user.applications.includes(id) : false);
 
     async function onClickApply(e) {
         e.preventDefault();
-        try{
+        try {
             const applied = await JoblyApi.applyToJob(user.username, id);
             if (applied) {
                 user.applications.push(id);
                 setUser(user);
                 setAppliedTo(true);
             }
-        } catch(e) {
+        } catch (e) {
             console.log(e);
             alert("Error applying to job");
         }
@@ -27,11 +27,15 @@ function Job({ job }) {
         <Card>
             <CardBody>
                 <div>ID: {id}</div>
-                {companyName?<div>Company: {companyName}</div>:""}
+                {companyName ? <div>Company: {companyName}</div> : ""}
                 <div>Title: {title}</div>
                 <div>Salary: {salary}</div>
-                {equity?<div>Equity: {equity}</div>:""}
-                {isAppliedTo?<Button className="mt-2">Applied</Button>:<Button className="mt-2" color="primary" onClick={onClickApply}>Apply</Button>}
+                {equity ? <div>Equity: {equity}</div> : ""}
+                {user ?
+                    (isAppliedTo ?
+                        <Button className="mt-2">Applied</Button>
+                        : <Button className="mt-2" color="primary" onClick={onClickApply}>Apply</Button>)
+                    : ""}
             </CardBody>
         </Card>
     )
